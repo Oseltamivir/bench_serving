e5ecba804f7917087bb99c7f20be2aedf4c028f0
Oseltamivir <bryansg2013@gmail.com>
2026-01-04 22:59:20 -0800

    lower num steps

--- benchmark_serving.py
+++ benchmark_serving.py
@@ -640,7 +640,7 @@ async def benchmark(
                                          output_len=test_output_len,
                                          logprobs=logprobs,
                                          best_of=best_of,
-                                         extra_body={"num_steps": 5, "merge_profiles": True, "profile_by_stage": True},
+                                         extra_body={"num_steps": 2, "merge_profiles": True, "profile_by_stage": True},
                                          multi_modal_content=test_mm_content,
                                          ignore_eos=ignore_eos)
         profile_output = await request_func(request_func_input=profile_input)
